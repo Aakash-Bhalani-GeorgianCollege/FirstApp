b841bf03c694c56d461140c2b81a5253bd0f6a1f
singhsaab97 <kanwarabhijitsingh@gmail.com>
2023-07-05 01:11:29 -0400

    added second screen

--- FirstApp/src/components/FlexBox1.tsx
+++ FirstApp/src/components/FlexBox1.tsx
@@ -1,22 +1,16 @@
-import { SafeAreaView, View, Text, StyleSheet } from "react-native"
+import { SafeAreaView, View, StyleSheet } from "react-native"
 
 const FlexBox1 = ()=> {
     return (
         <SafeAreaView style = {styles.wrapper}>
             <View style = {[styles.primary, styles.box1]}>
-                <View style = {styles.default}>
-                    <Text>Some text here</Text>
-                </View>
+                <View style = {styles.default}/>
             </View>
             <View style = {[styles.primary, styles.box2]}>
-                <View style = {styles.default}>
-                    <Text>Some text here</Text>
-                </View>
+                <View style = {styles.default}/>
             </View>
             <View style = {[styles.primary, styles.box3]}>
-                <View style = {styles.default}>
-                    <Text>Some text here</Text>
-                </View>
+                <View style = {styles.default}/>
             </View>
         </SafeAreaView>
     )
@@ -29,12 +23,12 @@ const styles = StyleSheet.create({
     },
     primary: {
         width: "100%",
-        height: 70,
+        height: 80,
         padding: 15
     },
     default: {
         flex: 1,
-        backgroundColor: "#ededed"
+        backgroundColor: 'red'
     },
     box1: {
         flex: 0.6
