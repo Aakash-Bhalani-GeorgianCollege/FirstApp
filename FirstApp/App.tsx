7b11cab79797e1b5c66336bdc820926319e1be1f
Kosiso Abone <abonekosiso@gmail.com>
2023-07-06 23:41:53 -0400

    resolve conflict

--- FirstApp/App.tsx
+++ FirstApp/App.tsx
@@ -1,19 +1,21 @@
-import { StyleSheet, View } from 'react-native';
+import { SafeAreaView, StyleSheet, View } from 'react-native';
 import FlexBox2 from './src/components/FlexBox2'
+import { DARK_COLORS } from './src/styles/Colors';
+import FavoriteScreen from './src/screens/FavoriteScreen';
 import CityWeatherDetails from './src/components/CityWeatherDetails/CityWeatherDetails';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      {/* <FlexBox2/> */}
-      <CityWeatherDetails />
-    </View>
+    <SafeAreaView style={styles.container}>
+      <FavoriteScreen/>
+      <CityWeatherDetails/>
+    </SafeAreaView>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: DARK_COLORS.PRIMARY_BACKGROUND_COLOR,
   },
 });
