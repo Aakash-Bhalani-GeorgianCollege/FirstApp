a318e96f44321e77ff9af5b169ae907756f053ee
Aakash Bhalani <aakashbhalani3476@gmail.com>
2023-07-07 21:55:10 -0400

    Updated project

--- FirstApp/App.tsx
+++ FirstApp/App.tsx
@@ -2,13 +2,22 @@ import { SafeAreaView, Settings, StyleSheet, View } from 'react-native';
 import { DARK_COLORS } from './src/styles/Colors';
 import SettingsPage from './src/components/SettingsPage';
 import HomePage from './src/components/HomePage';
+import Searchweather from './src/components/Searchweather';
+import FavoriteScreen from './src/screens/FavoriteScreen';
+import CityWeatherDetails from './src/components/CityWeatherDetails/CityWeatherDetails';
+import Contact from './src/components/Contact';
 
 
 export default function App() {
   return (
     <SafeAreaView style={styles.container}>
       {/* <FravoriteScreen/> */}
+      <HomePage/>
+      {/* <Searchweather/>
+      <FavoriteScreen/>
+      <CityWeatherDetails/>
       <SettingsPage/>
+      <Contact/> */}
     </SafeAreaView>
   );
 }
