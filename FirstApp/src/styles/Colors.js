794071922e77d1e4717ba6ee4bf5e721abe798bd
builder-jc4949 <200558522@student.georgianc.on.ca>
2023-08-04 20:46:11 -0400

    Color style updates (added gray palette)

--- FirstApp/src/styles/Colors.js
+++ FirstApp/src/styles/Colors.js
@@ -20,3 +20,16 @@ export const LIGHT_COLORS = {
     PRIMARY_BACKGROUND_COLOR: "#FFFFFF",
     SECONDARY_BACKGROUND_COLOR: "#2B2B2B"
 }
+
+//Mimicking the bootstrap color template
+export const GRAY = {
+    s100: "#f8f9fa",
+    s200: "#e9ecef",
+    s300: "#dee2e6",
+    s400: "#ced4da",
+    s500: "#adb5bd",
+    s600: "#6c757d",
+    s700: "#495057",
+    s800: "#343a40",
+    s900: "#212529"
+}
