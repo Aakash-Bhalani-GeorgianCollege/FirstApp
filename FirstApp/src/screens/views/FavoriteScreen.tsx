c38ebfcc0873091192a0b3c8ab0b64a16c749198
builder-jc4949 <200558522@student.georgianc.on.ca>
2023-10-29 14:53:25 -0400

    Changed view data ref to respective controllers

--- FirstApp/src/screens/views/FavoriteScreen.tsx
+++ FirstApp/src/screens/views/FavoriteScreen.tsx
@@ -2,45 +2,11 @@ import { ScrollView, Text } from "react-native"
 import { favoritePageStyles } from "../../styles/styles"
 import WeatherCard from "../../components/WeatherCard/WeatherCard"
 import { WeatherRecord } from "../../components/WeatherCard/WeatherRecord"
+import { getFavoriteDataFromModel } from "../controllers/screenDataControllers/FavoriteDataController"
 
 
 
-
-const testData = [
-    {
-        recordData: {
-            cityName: "Barrie",
-            timestamp: (new Date("2023-02-02 09:00:00")).getTime(),
-            tempSnapshot: 5,
-            maxTemp: 10,
-            minTemp: -10,
-        },
-        imgPath: require("../assets/images/grey-cloud.png"),
-        containerStyle: "warning"
-    },
-    {
-        recordData: {
-            cityName: "Barrie",
-            timestamp: (new Date("2023-02-02 12:00:00")).getTime(),
-            tempSnapshot: 10,
-            maxTemp: 10,
-            minTemp: -10,
-        },
-        imgPath: require("../assets/images/grey-cloud.png"),
-        containerStyle: "warning"
-    },
-    {
-        recordData: {
-            cityName: "Barrie",
-            timestamp: (new Date("2023-02-02 22:00:00")).getTime(),
-            tempSnapshot: -7,
-            maxTemp: 10,
-            minTemp: -10,
-        },
-        imgPath: require("../assets/images/black-cloud.png"),
-        containerStyle: "danger"
-    }
-]
+const testData = getFavoriteDataFromModel();
 
 const FavoriteScreen = () => {
     return (
